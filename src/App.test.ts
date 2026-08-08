128c78660af4078b88dfa69a547c6df31ccc61af
Isham Faizal <ishamf@users.noreply.github.com>
2026-08-08 12:46:52 +0300

    Update formatting

--- src/App.test.ts
+++ src/App.test.ts
@@ -1,7 +1,7 @@
-import { describe, expect, it } from 'vitest'
+import { describe, expect, it } from 'vitest';
 
 describe('test setup', () => {
   it('runs with Vitest', () => {
-    expect(true).toBe(true)
-  })
-})
+    expect(true).toBe(true);
+  });
+});
